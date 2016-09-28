ed95dcc48c3686f37bcc26c056f7ea41dc555ba7
liudongxin <153264502@qq.com>
2016-09-28 15:44:02 +0800

    commit and push
    commit

---
+++ testProject/testProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testProject
+//
+//  Created by liudx on 16/9/28.
+//  Copyright © 2016年 Lidosx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
